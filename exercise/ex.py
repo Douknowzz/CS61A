a0ec314419729fcbef5c0d06440e0f072e7a41c8
Douknowzz <jose1919c@gmail.com>
2024-07-25 21:15:47 +0800

    lab01 Q1

--- exercise/ex.py
+++ exercise/ex.py
@@ -1,6 +1,7 @@
 """ Our first Python source file. """
 
 from operator import floordiv, mod
+from math import pi, sqrt
 
 def divide_exact(n, d):
     """Return the quotient and remainder of dividing N by D.
@@ -21,3 +22,58 @@ def absolute_value(x):
         return 0
     else:
         return x
+
+def fib(n):
+    """Compute the nth Fibonacci number, for N >= 1."""
+    pred, curr = 1, 0
+    k = 0
+    while k < n:
+        pred, curr = curr, pred + curr
+        k = k + 1
+    return curr
+
+def identity(k):
+    return k
+
+def cube(k):
+    return pow(k, 3)
+
+def summation(n, term):
+    """Sum the first N terms of a sequence
+
+    >>> summation(5, cube)
+    225
+    """
+    total, k = 0, 1
+    while k <= n:
+        total, k = total + term(k), k + 1
+    return total
+
+def sum_naturals(n):
+    """Sum the first N natural numbers.
+
+    >>> sum_naturals(5)
+    15
+    """
+    return summation(n, identity)
+
+def sum_cubes(n):
+    """Sum the first N cubes of natural numbers.
+
+    >>> sum_cubes(5)
+    225
+    """
+    return summation(n, cube)
+
+def area(r, shape_constant):
+    assert r > 0, 'A length must be positive'
+    return r * r * shape_constant
+
+def area_square(r):
+    return area(r, 1)
+
+def area_circle(r):
+    return area(r, pi)
+
+def area_hexagon(r):
+    return area(r, 3 * sqrt(3) / 2)
